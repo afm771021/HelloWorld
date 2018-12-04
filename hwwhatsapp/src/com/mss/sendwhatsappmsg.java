895f02a6e662b6fcff8acd2dfc21ce5418bdfdb8
afm771021 <afm@mss.mx>
2018-12-04 13:56:27 -0600

    Update sendwhatsappmsg.java

--- hwwhatsapp/src/com/mss/sendwhatsappmsg.java
+++ hwwhatsapp/src/com/mss/sendwhatsappmsg.java
@@ -15,7 +15,9 @@ public class sendwhatsappmsg {
         Message message = Message.creator(
         		new com.twilio.type.PhoneNumber("whatsapp:+5215515104658"), 
                 new com.twilio.type.PhoneNumber("whatsapp:+14155238886"), 
-                "Envio de mensaje Whatsapp desde java")
+                
+                "Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10, 2019. Details: http://www.yummycupcakes.com/") 
+
             .create();
 
         System.out.println("confirmacion de mensaje whatsapp:" + message.getSid());
